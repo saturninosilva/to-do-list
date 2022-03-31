5db426f3d7e7fae56b375252f840823863fbed39
Saturnino Silva <torpedo77@gmail.com>
2022-03-30 22:26:07 -0300

    atualizado algoritmo

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Uma To Do List para exercicio do curso de desenvolvimento Front End do SENAI RJ
 2. [x] clonar o repositorio
 3. [x] Adicionar um input para digitar a tarefa
 4. [x] Adicionar um botao para adicionar a tarefa na lista
-5. [] Criar uma lista de tarefa
+5. [x] Criar uma lista de tarefa
 6. [] Remover a tarefa da lista quando marcar quando concluido
 
 
 Algoritmo para adicionar uma tarefa na lista
 1. [x] Ao clicar no botao pega o valor do input e armazenar o valor em uma variavel;
-
+2. [x] criar um item de lista e adicionar na lista de tarefas
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,25 @@
-const btnAddTodo = document.querySelector("#btn-add-todo");
+function getItem(item) {
+    return document.querySelector(item);
+}
+
+const btnAddTodo = getItem("#btn-add-todo");
 btnAddTodo.addEventListener("click", (evento) => {
-    const inputTodo = document.querySelector("#input-todo");
+    const inputTodo = getItem("#input-todo");
     const inputValue = inputTodo.value;
 
     console.log(inputValue);
+    if (inputValue == "") {
+        console.log("String vazia");
+    } else {
+        const todoList = getItem("#todo-list");
+
+        const listItem = document.createElement("li"); // <li></li>
+        const textItem = document.createTextNode(inputValue); //Texto criado
+    
+        listItem.appendChild(textItem); //<li>mais uma tarefa</li>
+        listItem.classList.add("list-group-item");
+
+        todoList.appendChild(listItem); //<ul>.....<li>mais uma tarefa</li></ul>
+        inputTodo.value = "";
+    }
 });
