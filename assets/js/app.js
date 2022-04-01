bcd2f26252e96f0f87027f1561ba7866c2a23f2c
Saturnino Silva <torpedo77@gmail.com>
2022-03-31 21:02:36 -0300

    corrigido código

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,31 @@ function getItem(item) {
     return document.querySelector(item);
 }
 
+function addTodo(todoText){
+    const div = document.createElement("div");
+    div.classList.add("form-check");
+
+    const input = document.createElement("input");
+    input.type = 'checkbox';
+    input.classList.add("form-check-input");
+
+    const label = document.createElement("label");
+    label.classList.add("form-check-label");
+    
+    const textItem = document.createTextNode(todoText); //Texto criado
+    
+    label.appendChild(textItem);
+    div.appendChild(input);
+    div.appendChild(label);
+
+    const listItem = document.createElement("li"); // <li></li>
+    listItem.appendChild(div); //<li>mais uma tarefa</li>
+    listItem.classList.add("list-group-item");
+
+    const todoList = getItem("#todo-list");
+    todoList.appendChild(listItem); //<ul>.....<li>mais uma tarefa</li></ul>
+}
+
 const btnAddTodo = getItem("#btn-add-todo");
 btnAddTodo.addEventListener("click", (evento) => {
     const inputTodo = getItem("#input-todo");
@@ -11,15 +36,8 @@ btnAddTodo.addEventListener("click", (evento) => {
     if (inputValue == "") {
         console.log("String vazia");
     } else {
-        const todoList = getItem("#todo-list");
-
-        const listItem = document.createElement("li"); // <li></li>
-        const textItem = document.createTextNode(inputValue); //Texto criado
-    
-        listItem.appendChild(textItem); //<li>mais uma tarefa</li>
-        listItem.classList.add("list-group-item");
-
-        todoList.appendChild(listItem); //<ul>.....<li>mais uma tarefa</li></ul>
+        addTodo(inputValue);
+       
         inputTodo.value = "";
     }
 });
